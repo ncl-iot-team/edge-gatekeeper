224f01ddd5bb287bf8b3fa8dfbc929f4b19f7397
Nipun Balan Thekkummal <nipunbalan@gmail.com>
2018-10-05 01:23:38 +0100

    Added log forwarding

--- forwarder/counter.go
+++ forwarder/counter.go
@@ -1,8 +1,10 @@
 package forwarder
 
 import (
-	"log"
+	"fmt"
 	"time"
+
+	"github.com/spf13/viper"
 )
 
 //var count int64
@@ -10,38 +12,37 @@ import (
 // NewRateCounter initiates a new counter
 func NewRateCounter(window time.Duration, countChannel chan bool) {
 
+	var amqpConnDetails AMQPConnDetailsType
+
+	amqpConnDetails.host = viper.GetString("remote-log-collector.main.host")
+	amqpConnDetails.port = viper.GetString("remote-log-collector.main.port")
+	amqpConnDetails.user = viper.GetString("remote-log-collector.main.user")
+	amqpConnDetails.password = viper.GetString("remote-log-collector.main.password")
+	amqpConnDetails.queue = viper.GetString("remote-log-collector.main.queue")
+
 	var count int64
-	tick := make(chan bool)
+
+	deliveries := make(chan string, 4096)
+
+	go NewProducer(amqpConnDetails, &deliveries)
 
 	// Function generates ticks at the end of the configured window
 	go func() {
 		for {
 			time.Sleep(window)
-			tick <- true
-		}
-	}()
-
-	// Fucntion records the count when a tick signal is received
-	go func() {
-		for range tick {
-			recordCount(window, &count)
+			recordCount(window, &count, &deliveries)
 		}
 	}()
 
 	for range countChannel {
-
 		count++
 	}
 
 }
 
-//func increment() {
-//	log.Printf("Increment Count: %d", count)
-//	count
-//}
-
-func recordCount(window time.Duration, count *int64) {
-	log.Printf("Count: %d", *count)
+func recordCount(window time.Duration, count *int64, deliveries *chan string) {
+	//	log.Printf("Count: %d", *count)
+	*deliveries <- fmt.Sprintf("%s | E1 Sent Count: %d", time.Now().Format(time.RFC3339), *count)
 	*count = 0
 
 }
